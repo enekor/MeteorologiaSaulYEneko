2e19b02882b9d1a9d80e10edbf07cadfc890ede2
Saul <saul.mellado@alumno.iesluisvives.org>
2021-10-20 16:29:46 +0200

    Corrección Media Mensual
    Media mensual divide por los valores que existen, no entre 24

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,7 @@ public class Main {
         System.out.println(c.calidadList.size());
         Filtro f = new Filtro();
         MediasHoras m = new MediasHoras();
-        System.out.println(m.mediaCalidadAire(f.filtroCalidadAire("9","14",c.calidadList)));
+        System.out.println(f.filtroCalidadAire("92","10",c.calidadList));
+        System.out.println(m.mediaCalidadAire(f.filtroCalidadAire("92","10",c.calidadList)));
     }
 }
