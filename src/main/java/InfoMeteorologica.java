2f8eddbe45188eb215189004614a11f593e81281
Eneko Rebollo <eneko12rebollo@gmail.com>
2021-10-21 20:02:17 +0200

    comentado el nuevo archivo java

--- src/main/java/InfoMeteorologica.java
+++ src/main/java/InfoMeteorologica.java
@@ -13,12 +13,12 @@ public class InfoMeteorologica {
     Filtro f = new Filtro();
 
     /**
-     *
-     * @param datosAire
-     * @param datosMeteo
-     * @param municipio
-     * @param magnitud
-     * @return
+     *dependiendo de los valores de municipio y magnitud llama a un metodo u otro pasandole por parametro los valores necesarios para que este funcione
+     * @param datosAire lista recien hecha y para filtrar de objetos CalidadAire
+     * @param datosMeteo lista recien hecha y para filtrar de objetos DatosMeteorologicos
+     * @param municipio codigo del municipio a mirar
+     * @param magnitud codigo de la magnitud que queremos sacar
+     * @return la respuesta de cada metodo, y, en caso de que la lista sea nula ya que en dicho municipio no hay los datos requeridos, un mensaje personalizado
      */
     public String proveedorDeDatos(List<CalidadAire> datosAire, List<DatosMeteorologicos> datosMeteo,String municipio, String magnitud){
         String returner="";
@@ -53,22 +53,22 @@ public class InfoMeteorologica {
     }
 
     /**
-     *
-     * @param datos
-     * @param municipio
-     * @param magnitud
-     * @return
+     *llama al metodo que filtra pasandole los parametros que se necesitan
+     * @param datos lista de DatosMeteorologicos a filtrar
+     * @param municipio que queremos filtrar
+     * @param magnitud que queremos obtener
+     * @return una string en orden media max min
      */
     private String datosMeteorologicos(List<DatosMeteorologicos> datos, String municipio, String magnitud){
         return mh.mediadatosMeteo(f.filtroDatosMeteo(municipio,magnitud,datos));
     }
 
     /**
-     *
-     * @param datos
-     * @param municipio
-     * @param magnitud
-     * @return
+     *llama al metodo que filtra pasandole los parametros que se necesitan
+     * @param datos lista de CalidadAire a filtrar
+     * @param municipio que queremos filtrar
+     * @param magnitud que queremos obtener
+     * @return una string en orden media max min
      */
     private String datosCalidad(List<CalidadAire> datos,String municipio, String magnitud){
         return mh.mediaCalidadAire(f.filtroCalidadAire(municipio,magnitud,datos));
