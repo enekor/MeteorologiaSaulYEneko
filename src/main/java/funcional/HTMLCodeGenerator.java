8a5d440cc6d8bf464dc6f507c5b5d04009aaf75e
Saul <saul.mellado@alumno.iesluisvives.org>
2021-10-23 12:08:01 +0200

    ArregloCodigo
    FreeChart y metodo texto html

--- src/main/java/funcional/HTMLCodeGenerator.java
+++ src/main/java/funcional/HTMLCodeGenerator.java
@@ -1,3 +1,16 @@
 package funcional;
 public class HTMLCodeGenerator {
+
+    String respuesta;
+    /*
+    private String devolverMagnitudes(){
+
+        for (int i = 0; i<=89;i++){
+            if(map.containsKey(i)){
+                respuesta+=metodo()+"/n";
+            }
+        }
+        return respuesta;
+    }
+    */
 }
