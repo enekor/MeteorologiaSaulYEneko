ed960ae4de7e07616c82b744ecc1be1f940d9771
Saul <saul.mellado@alumno.iesluisvives.org>
2021-10-24 13:31:34 +0200

    FreeChart Completado
    Añadido el histograma a precipitación

--- src/main/java/JfreeChart/FreeChart.java
+++ src/main/java/JfreeChart/FreeChart.java
@@ -4,6 +4,7 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.statistics.HistogramDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
@@ -25,26 +26,45 @@ import java.util.List;
         }
 
         public String generarPng(List<Double> valores, String nombre) throws IOException {
-            var prueba = new XYSeries("2021");
-            for (int i = 0; i < valores.size(); i++) {
-                prueba.add(i, valores.get(i));
-            }
-            var dataset = new XYSeriesCollection();
-            dataset.addSeries(prueba);
+            String path = System.getProperty("user.dir") + File.separator + "src" + File.separator + "main" + File.separator + "resources" + File.separator + "graficas" + File.separator + nombre + ".png";
+            if (nombre.equalsIgnoreCase("precipitacion")) {
+                HistogramDataset dataset = new HistogramDataset();
+                double[] prueba = new double[valores.size()];
+                for (int i = 0; i < valores.size(); i++) {
+                    prueba[i] = valores.get(i);
+                }
+                dataset.addSeries("key", prueba, 50);
 
-            JFreeChart chart = ChartFactory.createXYLineChart(
-                    nombre,
-                    "Dias",
-                    "Magnitud",
-                    dataset,
-                    PlotOrientation.VERTICAL,
-                    true,
-                    true,
-                    false
-            );
-            String path = System.getProperty("user.dir")+File.separator+"src"+File.separator+"main"+File.separator+"resources"+File.separator+"graficas"+File.separator+nombre+".png";
-            ChartUtilities.saveChartAsPNG(new File(path), chart, 450, 400);
-            return path;
-        }
+                JFreeChart histograma = ChartFactory.createHistogram("Histograma Precipitación",
+                        "Dias",
+                        "Magnitud",
+                        dataset,
+                        PlotOrientation.VERTICAL,
+                        true,
+                        true,
+                        false);
+                ChartUtilities.saveChartAsPNG(new File(path), histograma, 450, 400);
+            } else {
+                var prueba = new XYSeries("2021");
+                for (int i = 0; i < valores.size(); i++) {
+                    prueba.add(i, valores.get(i));
+                }
+                var dataset = new XYSeriesCollection();
+                dataset.addSeries(prueba);
+
+                JFreeChart chart = ChartFactory.createXYLineChart(
+                        nombre,
+                        "Dias",
+                        "Magnitud",
+                        dataset,
+                        PlotOrientation.VERTICAL,
+                        true,
+                        true,
+                        false
+                );
+                ChartUtilities.saveChartAsPNG(new File(path), chart, 450, 400);
+            }
+                return path;
+            }
     }
 
